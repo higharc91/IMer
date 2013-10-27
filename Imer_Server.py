e2ea780c2cd343ea322a977c9f6e28458695c4d2
higharc91 <higharc91@gmail.com>
2013-10-27 09:21:35 -0400

    Create Imer_Server.py
    The server that is started when no previous server is found.

---
+++ Imer_Server.py
@@ -0,0 +1,42 @@
+import socket, threading, time
+
+
+class IM_Server():
+    """The main goal is to call this class when IMER can not
+       a current active server, thus making one locally"""
+    def __init__(self):
+        self.clientlist = [] #stores client connections
+        self.sockserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #tcp conn init
+        self.sockserver.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1) #closes port when done
+
+    def __call__(self):
+        return self   #THis is what you need to call
+
+    def listen(self, ip):
+        self.sockserver.bind((ip, 11117))
+        self.sockserver.listen(5)
+        print('Server started on %s' %ip)
+        while True:
+            try:
+                client, addr = self.sockserver.accept()
+                self.clientlist.append(client)
+                print('client Connected:', client.getpeername())  #all these added prints remove
+                threading.Thread(target=self.handler, args=(client,)).start()
+            except:
+                continue
+
+    def handler(self,client):
+        while True:
+            data = client.recv(1024)
+            if data:  #checks to see if there is data
+                threading.Thread(target=self.responder, args=(data,)).start()
+            else:
+                continue
+
+    def responder(self,data):
+        for c in self.clientlist:
+            try:
+                c.send(data)
+            except:
+                continue
+
